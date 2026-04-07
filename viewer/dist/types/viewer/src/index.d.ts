7ddec6620b895d899dfbb261665bd44aa0de31d5
Julien Paoletti <julien.paoletti@protonmail.com>
2026-04-08 00:12:30 +0200

    chore: remove viewer/dist from version control

--- viewer/dist/types/viewer/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { ArchieViewer } from './viewer';
-export type { ViewerOptions } from './viewer';
-export type { SerializedDiagram, SerializedComponent, SerializedConnection, SerializedViewport } from '../../src/editor/editorTypes';
